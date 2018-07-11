a45de7670c81ddec7c429089a441391cf009701a
Josue V. Alorro <joalorro@gmail.com>
2018-07-11 17:35:03 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -38,16 +38,14 @@ def play(my_songs)
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
   
-  puts "Please enter a song name or number:"
+  puts "Please enter a song name:"
   input = gets.chomp
   
-  tracks = 1..song_list.length
+  tracks = 1..my_songs.length
   
-  if song_list.include?(input) == true 
+  if my_songs.include?(input) == true 
     puts "Playing /#{song_list[ song_list.index(input) ]}/"
-  elsif tracks.include?(input.to_i) == true 
-    puts "Playing #{song_list[input.to_i - 1]}"
-  else
+  else 
     puts "Invalid input, please try again."
   end
   
